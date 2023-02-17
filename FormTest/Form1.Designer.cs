f2ee3d1611fcd75073a88e1f68cb26361c7b3a93
jk <jkite>
2023-02-17 12:33:46 -0800

    Changed image size

--- FormTest/Form1.Designer.cs
+++ FormTest/Form1.Designer.cs
@@ -43,10 +43,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FormTest.Properties.Resources.baer;
-            this.pictureBox1.Location = new System.Drawing.Point(341, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(341, 15);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 62);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(125, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
